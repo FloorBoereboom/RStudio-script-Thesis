a1357d5018506fde9cd7e0c7c5f7ac00c427d0b9
FloorBoereboom <floorb7@hotmail.com>
2026-05-21 19:28:03 +0200

    Update Overlap DEGs.R

--- Overlap DEGs.R
+++ Overlap DEGs.R
@@ -1,22 +1,22 @@
-# The calculation of overlap between two Pt-based compound corresponding DEG tables
+# Retreive number of significant DEGs overlapping between two Pt-based compounds
 #1. Import complete DEGs tables (including non-significant values)
-cis <- read.delim("data/cisplatin_full_DEGs.txt")
+cis <- read.delim("data/cisplatin_full_DEGs.txt") 
 car <- read.delim ("data/carboplatin_full_DEGs.txt")
 ox <- read.delim("data/oxaliplatin_full_DEGs.txt")
 
 
 #Retreive overlap of DEGs between compounds cisplatin and carboplatin
 merge1 <- merge(cis, car, by = "GeneID")
-sig1 <- merge1[merge1$padj.x < 0.01, ]
-sig2 <- sig1[sig1$padj.y < 0.01, ]
+sig1 <- merge1[merge1$padj.cis < 0.01, ]
+sig2 <- sig1[sig1$padj.car < 0.01, ]
 
 
 #Retreive overlap of DEGs between compounds cisplatin and oxaliplatin
 merge2 <- merge(cis, ox, by = "GeneID")
-sig1 <- merge2[merge2$padj.x < 0.01, ]
-sig2 <- sig1[sig1$padj.y < 0.01, ]
+sig1 <- merge2[merge2$padj.cis < 0.01, ]
+sig2 <- sig1[sig1$padj.car < 0.01, ]
 
 #Retreive overlap of DEGs between compounds carboplatin and oxaliplatin
 merge3 <- merge(car, ox, by = "GeneID")
-sig1 <- merge3[merge3$padj.x < 0.01, ]
-sig2 <- sig1[sig1$padj.y < 0.01, ]
+sig1 <- merge3[merge3$padj.car < 0.01, ]
+sig2 <- sig1[sig1$padj.ox < 0.01, ]
